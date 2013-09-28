8d4d5cc5d9abd2bd293147b32e0c1cdf6844c636
Venkat Dinavahi <vendiddy@gmail.com>
2013-09-28 19:26:31 -0400

    added user and email for travis ci

--- spec/support/git_extras.rb
+++ spec/support/git_extras.rb
@@ -9,6 +9,8 @@ module Code
         Dir.chdir repo_path
 
         System.call 'init'
+        System.call 'config user.email "test@hotmail.com"'
+        System.call 'config user.name "Chuck Norris"'
         System.exec 'touch README'
         System.call 'add -A'
         System.call 'commit -m "initial commit"'
