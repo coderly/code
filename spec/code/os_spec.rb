c13c74129615e56c3834fd67185db2d72c6ff54e
begedin <begedinnikola@gmail.com>
2015-04-17 17:22:32 +0200

    Moved os spec to proper module

--- spec/code/os_spec.rb
+++ spec/code/os_spec.rb
@@ -1,48 +1,50 @@
 require 'code/os'
 
 module Code
-  describe "OS" do
-    it "detects linux" do
-      platform_string ="x86_64-linux"
-      os_detector = Code::System::OS.new(platform_string: platform_string)
-
-      expect(os_detector.linux?).to be true
-    end
-
-    it "detects windows" do
-      platform_string="i386-mingw32"
-      os_detector = Code::System::OS.new(platform_string: platform_string)
-
-      expect(os_detector.windows?).to be true
-    end
-
-    it "detects unix" do
-      platform_string ="x86_64-linux"
-      os_detector = Code::System::OS.new(platform_string: platform_string)
-
-      expect(os_detector.unix?).to be true
-    end
-
-    it "detects osx" do
-      platform_string = "x86_64-darwin14"
-      os_detector = Code::System::OS.new(platform_string: platform_string)
-
-      expect(os_detector.osx?).to be true
-    end
-
-    describe "#open_command" do
-      it "returns 'open' for osx" do
-        platform_string ="x86_64-darwin14"
-        os_detector = Code::System::OS.new(platform_string: platform_string)
-
-        expect(os_detector.open_command).to eq "open"
-      end
-
-      it "returns 'xdg-open' for linux" do
+  module System
+    describe OS do
+      it "detects linux" do
         platform_string ="x86_64-linux"
         os_detector = Code::System::OS.new(platform_string: platform_string)
 
-        expect(os_detector.open_command).to eq "xdg-open"
+        expect(os_detector.linux?).to be true
+      end
+
+      it "detects windows" do
+        platform_string="i386-mingw32"
+        os_detector = Code::System::OS.new(platform_string: platform_string)
+
+        expect(os_detector.windows?).to be true
+      end
+
+      it "detects unix" do
+        platform_string ="x86_64-linux"
+        os_detector = Code::System::OS.new(platform_string: platform_string)
+
+        expect(os_detector.unix?).to be true
+      end
+
+      it "detects osx" do
+        platform_string = "x86_64-darwin14"
+        os_detector = Code::System::OS.new(platform_string: platform_string)
+
+        expect(os_detector.osx?).to be true
+      end
+
+      describe "#open_command" do
+        it "returns 'open' for osx" do
+          platform_string ="x86_64-darwin14"
+          os_detector = Code::System::OS.new(platform_string: platform_string)
+
+          expect(os_detector.open_command).to eq "open"
+        end
+
+        it "returns 'xdg-open' for linux" do
+          platform_string ="x86_64-linux"
+          os_detector = Code::System::OS.new(platform_string: platform_string)
+
+          expect(os_detector.open_command).to eq "xdg-open"
+        end
       end
     end
   end
