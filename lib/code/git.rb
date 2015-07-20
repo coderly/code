ce54dd769b471a57055588cd1c6bfacb3fc6827c
Nikola Begedin <begedinnikola@gmail.com>
2015-07-20 09:33:38 +0200

    Fix search command broken by merge

--- lib/code/git.rb
+++ lib/code/git.rb
@@ -104,7 +104,7 @@ module Code
     end
 
     def search
-      System.open_in_browser "https://github.com/#{current_repo}/find/development"
+      System.open_in_browser "https://github.com/#current_repo_slug/find/development"
     end
 
     def create_feature_pr(base, message)
@@ -173,6 +173,10 @@ module Code
 
     def github_api
       @github_api ||= GitHubAPI.new
+    end
+
+    def current_repo_slug
+      Repository.current.slug
     end
 
   end
