8010603d9b27c2de01049b998d8c22787ba80a7e
Venkat Dinavahi <vendiddy@gmail.com>
2016-01-21 16:03:29 -0500

    Updated other IO comments to make use of STDIN and STDOUT. Updated tests to reflect this change.

--- lib/code/system.rb
+++ lib/code/system.rb
@@ -12,7 +12,7 @@ module Code
 
     def prompt(prompt_text)
       print prompt_text + ": "
-      input = STDIN.gets
+      input = gets
       input.strip
     end
 
@@ -56,8 +56,16 @@ module Code
       not $?.success?
     end
 
+    def print(text)
+      STDOUT.print text
+    end
+
     def puts(text)
-      Kernel.puts text
+      STDOUT.puts text
+    end
+
+    def gets
+      STDIN.gets
     end
 
     def noecho_gets
